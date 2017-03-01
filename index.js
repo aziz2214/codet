d57c8e315d8757ab1c3497cd0b089da9702bb12d
aziz2214 <ra2296@nyu.edu>
2017-03-01 13:58:42 -0500

    added socket.io & handlebars templ

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ var express = require('express')
 var handlebars = require('express-handlebars'); 
 var MongoClient = require('mongodb').MongoClient; 
 var bodyParser = require('body-parser'); 
-
 var app = express(); 
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(process.env.PORT || 3000);
+
+
+
+
+
+
 
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static('public')); 
@@ -13,21 +22,24 @@ app.engine('handlebars', handlebars( {
 })); 
 app.set('view engine', 'handlebars'); 
 
-
 var db; 
-
 MongoClient.connect('mongodb://admin:admin123@ds013206.mlab.com:13206/codet-db',  function(err, database){ 
     if (err) return console.log(err); 
     db = database; 
-    app.listen(process.env.PORT || 3000); 
+    server.listen(process.env.PORT || 3000); 
 }); 
 
-//
-//app.get('/', function(req, res) { 
-//    res.render('home'); 
-//})
 
 
+io.on('connection', function(client) {    // SOCKET.IO // 
+  client.on('message', function(data) {
+    console.log('message recieved', data);
+    client.broadcast.emit('message', data);
+  });
+
+});
+
+// --> Begin EXPRESS --------
 app.get('/', function(req, res) { 
     db.collection("notes").find().toArray(function(err, results) {
         res.render('home', {results: results}); 
@@ -54,5 +66,12 @@ app.post('/new', function(req, res) {
     
 }); 
 
+app.get('/live', function(req, res) { 
+    res.render('live'); 
+}); 
 
+app.post('/live', function(req, res) { 
+    console.log(req.body.username); 
+    socket.emit('message', req.body.username); 
+})
 
